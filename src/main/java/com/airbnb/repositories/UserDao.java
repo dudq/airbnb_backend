8b6b49ee8f222a2186b2308c6d3adaca233eef5e
NguyenVinh <nguyenquangvinh2518@gmail.com>
2020-01-09 10:23:02 +0700

    writen UserInformation

--- src/main/java/com/airbnb/repositories/UserDao.java
+++ src/main/java/com/airbnb/repositories/UserDao.java
@@ -1,4 +1,41 @@
 package com.airbnb.repositories;
 
+import com.airbnb.messages.response.UserInformation;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public class UserDao {
+    @PersistenceContext
+    private EntityManager em;
+
+    public UserInformation userInformation(Long userId) {
+        String sql = "select u.id,u.email,u.name,u.username,u.picture,r.name roleName\n" +
+                "from users u\n" +
+                "join  airbnb.user_roles ur\n" +
+                "on ur.user_id = u.id\n" +
+                "join roles r\n" +
+                "on r.id =  ur.role_id\n" +
+                "where u.id = :userId";
+
+        Query query = em.createNativeQuery(sql);
+        query.setParameter("userId", userId);
+        Object[] result = (Object[]) query.getSingleResult();
+
+        int i = 0;
+
+        UserInformation userInformation = new UserInformation();
+        userInformation.setId(Long.parseLong("" + result[i++]));
+        userInformation.setEmail("" + result[i++]);
+        userInformation.setName("" + result[i++]);
+        userInformation.setUsername("" + result[i++]);
+        userInformation.setPicture("" + result[i++]);
+        userInformation.setRole_name("" + result[i++]);
+        return userInformation;
+    }
 }
