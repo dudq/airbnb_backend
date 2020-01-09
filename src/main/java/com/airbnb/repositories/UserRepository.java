9814c3d6ce67a1bc6a500f260e966d5b10723b7a
NguyenVinh <nguyenquangvinh2518@gmail.com>
2020-01-09 10:11:54 +0700

    writen UserRepository

--- src/main/java/com/airbnb/repositories/UserRepository.java
+++ src/main/java/com/airbnb/repositories/UserRepository.java
@@ -1,4 +1,16 @@
 package com.airbnb.repositories;
 
-public interface UserRepository  {
+import com.airbnb.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
