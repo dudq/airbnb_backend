5bca761df446bd6b72278709fd3425b6835e5230
du <dudq.xmt@gmail.com>
2020-01-12 15:18:50 +0700

    add annotation repository

--- src/main/java/com/airbnb/repositories/HouseRepository.java
+++ src/main/java/com/airbnb/repositories/HouseRepository.java
@@ -2,9 +2,11 @@ package com.airbnb.repositories;
 
 import com.airbnb.models.House;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface HouseRepository extends JpaRepository<House, Long> {
     List<House> findByHost(Long hostId);
 
