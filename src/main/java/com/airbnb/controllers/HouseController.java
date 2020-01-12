db432406fde62d14e47e9e0d4170c40ea9c345ef
du <dudq.xmt@gmail.com>
2020-01-12 18:55:13 +0700

    add house controller

---
+++ src/main/java/com/airbnb/controllers/HouseController.java
@@ -0,0 +1,47 @@
+package com.airbnb.controllers;
+
+import com.airbnb.messages.response.HouseInformation;
+import com.airbnb.messages.response.ResponseMessage;
+import com.airbnb.security.sevice.UserPrinciple;
+import com.airbnb.services.HouseService;
+import com.airbnb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/houses")
+public class HouseController {
+
+    @Autowired
+    private HouseService houseService;
+
+    @Autowired
+    private UserService userService;
+
+    private UserPrinciple getCurrentUser() {
+        return (UserPrinciple) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ResponseMessage> listAllHouse(@RequestParam(value = "page", required = false) Integer page,
+                                                        @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        List<HouseInformation> houses = this.houseService.getListHouseInformation(1, 2);
+
+        if (houses.isEmpty()) {
+            return new ResponseEntity<ResponseMessage>(
+                    new ResponseMessage(false, "Not Found Data", null), HttpStatus.OK
+            );
+        }
+
+        return new ResponseEntity<ResponseMessage>(
+                new ResponseMessage(true, "Successfully. Get list all house", houses), HttpStatus.OK
+        );
+    }
+}
