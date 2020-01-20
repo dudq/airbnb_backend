3651b814da8cff602d70939cb3e56d010d59ccbc
NguyenVinh <nguyenquangvinh2518@gmail.com>
2020-01-20 09:06:31 +0700

    update Order House

--- src/main/java/com/airbnb/models/OrderHouse.java
+++ src/main/java/com/airbnb/models/OrderHouse.java
@@ -4,27 +4,29 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 public class OrderHouse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
     @NotEmpty
     private Date dateCheckOut;
     @NotEmpty
     private Date dateCheckIn;
     @NotEmpty
     private Integer guest;
-    @NotBlank
-    @Column(unique = true)
-    private String name;
 
-    @OneToMany(targetEntity = House.class)
-    private List<House> house;
+    @ManyToOne(targetEntity = House.class)
+    private House house;
     @ManyToOne(targetEntity = User.class)
-    private List<User> users;
+    private User users;
+
 
     public OrderHouse() {
     }
@@ -33,28 +35,20 @@ public class OrderHouse {
         this.name = name;
     }
 
-    public Integer getGuest() {
-        return guest;
-    }
-
-    public void setGuest(Integer guest) {
-        this.guest = guest;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Date getDateCheckOut() {
@@ -73,19 +67,27 @@ public class OrderHouse {
         this.dateCheckIn = dateCheckIn;
     }
 
-    public String getName() {
-        return name;
+    public Integer getGuest() {
+        return guest;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setGuest(Integer guest) {
+        this.guest = guest;
     }
 
-    public List<House> getHouse() {
+    public House getHouse() {
         return house;
     }
 
-    public void setHouse(List<House> house) {
+    public void setHouse(House house) {
         this.house = house;
+    }
+
+    public User getUsers() {
+        return users;
+    }
+
+    public void setUsers(User users) {
+        this.users = users;
     }
 }
