7b2f4400b1ab525b504452557963ab2c2d5688c0
du <dudq.xmt@gmail.com>
2020-02-08 16:03:07 +0700

    update category to permit all

--- src/main/java/com/airbnb/security/WebSecurityConfig.java
+++ src/main/java/com/airbnb/security/WebSecurityConfig.java
@@ -58,6 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/houses/**").permitAll()
+                .antMatchers("/api/categories/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
